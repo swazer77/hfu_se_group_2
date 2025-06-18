ffe1ab82f5138cd75fab658a3a78686dfcc38f9f
Christian Meier <cmeier88@outlook.com>
2025-06-18 21:18:04 +0200

    [DbAccess] add filters

--- 002_Umsetzung/ShopSync/DbAccess/DbClient.cs
+++ 002_Umsetzung/ShopSync/DbAccess/DbClient.cs
@@ -6,17 +6,23 @@ public class DbClient
 {
     public DbClient()
     {
-        //using (Context dbContext = new())
-        //{
-        //    dbContext.Database.EnsureDeleted();
-        //    dbContext.Database.EnsureCreated();
-        //}
+        using Context dbContext = new();
+        //dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
     }
 
     public List<DbProduct> GetAllProducts()
     {
         using Context dbContext = new ();
-        return dbContext.Product.ToList();
+        return dbContext.Product
+            .Where(p => p.ShopChanged != 'D' && p.ErpChanged != 'D').ToList();
+    }
+
+    public List<DbProduct> GetAllErpChanged()
+    {
+        using Context dbContext = new ();
+        return dbContext.Product
+            .Where(p => p.ShopChanged != 'D' && p.ErpChanged != 'D' && p.ErpChanged != null).ToList();
     }
 
     public void InsertProduct(DbProduct product)
