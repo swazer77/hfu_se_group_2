c1dd6a002d991dd61c2792f0bc5ebfcd83d198b4
Christian Meier <cmeier88@bluewin.ch>
2025-06-18 11:29:55 +0200

    [Model] remove json field names since this is configured in HttpAccess now

--- 002_Umsetzung/ShopSync/Model/Attributes.cs
+++ 002_Umsetzung/ShopSync/Model/Attributes.cs
@@ -1,13 +1,11 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Model;
+﻿namespace Model;
 
 public class Attributes
 {
     public List<Locale>? Locale { get; set; }
-    [JsonPropertyName("last_modified")] public string? LastModified { get; set; }
+    public string? LastModified { get; set; }
     public string? Created { get; set; }
     public double Price { get; set; }
-    [JsonPropertyName("live_from")] public string? LiveFrom { get; set; }
-    [JsonPropertyName("live_until")] public string? LiveUntil { get; set; }
+    public string? LiveFrom { get; set; }
+    public string? LiveUntil { get; set; }
 }
