d021a9037436351468493db727ba710b01ecb29d
Christian Meier <cmeier88@outlook.com>
2025-06-18 15:19:09 +0200

    [DbAccess] create project and empty context class

---
+++ 002_Umsetzung/ShopSync/DbAccess/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace DbAccess;
+
+public class Context : DbContext
+{
+    public DbSet<DBModel.DbProduct> Product { get; set; }
+    public DbSet<DBModel.DbLocale> Product { get; set; }
+    public DbSet<DBModel.DbProduct> Product { get; set; }
+    public DbSet<DBModel.DbProduct> Product { get; set; }
+
+}
