32ad7fa17642b5a74c20b34946d6b41b2294e17a
swazer77 <gamerpcdk@gmail.com>
2025-06-20 10:11:03 +0200

    [DbModel] add simple sql for getting a product by id

---
+++ 002_Umsetzung/ShopSync/DBModel/GetProductById.sql
@@ -0,0 +1,5 @@
+﻿select * from Product
+join Attributes on Product.id = Attributes.id
+join Locale on Product.id = Locale.DbAttributesId
+join Shop on Product.ShopID = Shop.id
+where product.ProductID = '111111111111113';
